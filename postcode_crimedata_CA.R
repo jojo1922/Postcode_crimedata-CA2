0ba096875edfa22e0c0f92a685b22c59c522243b
jojo1922 <44625291+jojo1922@users.noreply.github.com>
2019-04-12 22:20:18 +0100

    initial commit

--- postcode_crimedata_CA.R
+++ postcode_crimedata_CA.R
@@ -1,23 +1,43 @@
 getwd()
 setwd(dir = "D:/datascience project/postcode_crimedata_CA2")
-ni_postcode_data <-read.csv("NIPostcodes.csv") # Read the dataset called NIPostcodes.csv
-View(ni_postcode_data)
-nrow(ni_postcode_data)# find total number of rows
-#nip_postcode
-str(ni_postcode_data)# structure of dataset before cleaning
-head(ni_postcode_data)
-dim(ni_postcode_data)
-names(ni_postcode_data) 
-names(ni_postcode_data) <-c("Organisation Name","Sub-building Name","Building Name","Number","Primary Thorfare","Alt Thorfare","Secondary Thorfare","Locality",
-                            "Townland","Town","County","Postcode","x-coordinates","y-coordinates","Primary Key (identifier)")
-names(ni_postcode_data)
-nrow(ni_postcode_data)
-head(ni_postcode_data,10)# first 10 rows of the dataframe
-dim(ni_postcode_data)# dimension including total number of rows and column
-missing_data <- complete.cases(ni_postcode_data)
-missing_data
-sum(missing_data)
-ni_post_missing <- ni_postcode_data[complete.cases(ni_postcode_data),]
-ni_post_missingmissing_1 <- md.pattern(ni_postcode_data)
-ni_post_missingmissing_1
-dim(ni_post_missing)
+library(readr)
+NIPostcodes <- read_csv("NIPostcodes.csv")
+View(NIPostcodes)
+str(NIPostcodes)
+names(NIPostcodes) <- c("Organisation Name","Sub-building Name","Building Name","Number","Primary Thorfare","Alt Thorfare","Secondary Thorfare","Locality",
+                        "Townland","Town","County","Postcode","x-coordinates","y-coordinates","Primary Key (identifier)")# Adding suitable title for each attribute in the column
+names(NIPostcodes)
+NIPostcodes
+nrow(NIPostcodes) # total number of rows
+str(NIPostcodes) # structure of NIPostcode dataset
+head(NIPostcodes,10)
+is.na(NIPostcodes$`Organisation Name`)
+#na_vec <- c(which(complete.cases(NIPostcodes$`Organisation Name`)))
+#na_vec
+sum_of_na_organisation<- sum( is.na(NIPostcodes$`Organisation Name`))# sum of missing values in Organisation name col
+sum_of_na_organisation
+#NIPostcodes[is.na(NIPostcodes)]=0
+#View(NIPostcodes) # replace NA value with 0
+mean_organisation <- mean(is.na(NIPostcodes$`Organisation Name`)) #Mean of missing value of organisation name
+mean_organisation
+sum_of_na_subbulding<- sum( is.na(NIPostcodes$`Sub-building Name`))# sum of missing values in Organisation name col
+sum_of_na_subbulding
+mean_of_na_subbulding<- mean(is.na(NIPostcodes$`Sub-building Name`)) #Mean of missing value of organisation name
+mean_of_na_subbulding
+#sum_of_na_bulding_name <- sum(is.na(NIPostcodes$`Building Name`))
+
+names(NIPostcodes) <- c("Organisation Name","Sub-building Name","Building Name","Number","Primary Thorfare","Alt Thorfare","Secondary Thorfare","Locality",
+                        "Townland","Town","Categorising Factor","Postcode","x-coordinates","y-coordinates","Primary Key (identifier)")# Modify the County attribute to be a categorising factor. 
+names(NIPostcodes)
+NIPostcodes<-NIPostcodes[,c(15,1:14)]# move the last column named Primary key to the first column
+NIPostcodes
+
+
+
+new_data_1 <- subset(NIPostcodes, Town== 'LIMAVADY')# extract the dataframe contaning the town name LIMAVADY.
+new_data_1
+#Limavady_data <- subset(new_data_1$Locality,new_data_1$Townland,new_data$Town)
+#dfnew5 <- subset(diamonds, select=c("color", "carat", "price"))
+Limavady_data <-subset(new_data_1, select = c(9,10,11))# create a dataset called Limavady_data with locality,townland and town 
+Limavady_data
+write.csv(Limavady_data,"CleanNIPostcodeData.csv") #save data as CleanNIPostcodeData.csv
